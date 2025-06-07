9f913c94225dd099209e523364aff4a8c9767935
Godkiller98 <rohitmane9890@gmail.com>
2025-06-07 17:04:20 +0530

    testing git push through visual studio

--- src/StringPalindrome.java
+++ src/StringPalindrome.java
@@ -7,6 +7,8 @@ public class StringPalindrome {
 //        int[] a={3,1,2,4,5,5,4,3,2,6,7};
 //        int[] tar= Arrays.stream(a).sorted().distinct().toArray();
 //        System.out.println(Arrays.toString(tar));
+
+
     }
     public static boolean isPlain(String s){
         String sb1 = new StringBuffer(s).reverse().toString();
